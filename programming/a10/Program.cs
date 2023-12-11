c13b30b01e682e19b17920ecf3623ce9a403770f
Alex Bennett <ajbenn90@gmail.com>
2023-12-11 14:25:42 -0700

    Added a10 and a11 solution

---
+++ programming/a10/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+// it's just topological sort
+class Solution
+{
+    public static void Main()
+    {
+        string[] metadata = Console.ReadLine().Split(' ');
+        int numSticks = int.Parse(metadata[0]);
+        int numLines = int.Parse(metadata[1]);
+
+        // build dependency graph and get dependency counts
+        List<int>[] dependencyGraph = new List<int>[numSticks + 1];
+        for (int i = 0; i < dependencyGraph.Length; i++)
+            dependencyGraph[i] = new();
+        int[] dependencyCounts = new int[dependencyGraph.Length];
+        for (int i = 0; i < numLines; i++)
+        {
+            string[] dependency = Console.ReadLine().Split(' ');
+            int top = int.Parse(dependency[0]);
+            int bottom = int.Parse(dependency[1]);
+            dependencyGraph[top].Add(bottom);
+            dependencyCounts[bottom]++;
+        }
+
+        List<int> sol = PickUpSticks(dependencyGraph, dependencyCounts);
+        if (sol.Count < numSticks) // impossible to pick them all up
+            Console.WriteLine("IMPOSSIBLE");
+        else
+            foreach (int stick in sol)
+                Console.WriteLine(stick);
+    }
+
+    /// <summary>
+    /// Returns an order to remove as many sticks as possible without removing one with another stick on top of it
+    /// </summary>
+    /// <param name="dependencyGraph">A 1-indexed dependency graph where graph[i] is a list of sticks directly below stick i</param>
+    /// <param name="dependencyCounts">dependencyCounts[i] = the number of sticks directly above stick i (in-degree)</param>
+    /// <returns>An ordered list of all the sticks as described above</returns>
+    public static List<int> PickUpSticks(List<int>[] dependencyGraph, int[] dependencyCounts)
+    {
+        // remove a stick with nothing on top of it, update the sticks below it, repeat.
+        List<int> sol = new(dependencyGraph.Length - 1);
+        Queue<int> toRemove = new();
+        for (int i = 1; i < dependencyGraph.Length; i++)
+            if (dependencyCounts[i] == 0)
+                toRemove.Enqueue(i);
+        while (toRemove.Count > 0)
+        {
+            int currRemove = toRemove.Dequeue();
+            sol.Add(currRemove);
+            foreach (int stick in dependencyGraph[currRemove])
+                if (--dependencyCounts[stick] == 0)
+                    toRemove.Enqueue(stick);
+        }
+        return sol;
+    }
+}
---
+++ programming/a11/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+// it's just DFS from house 1
+class Solution
+{
+    public static void Main()
+    {
+        string[] metadata = Console.ReadLine().Split(' ');
+        int numHouses = int.Parse(metadata[0]);
+        int numCables = int.Parse(metadata[1]);
+
+        List<int>[] graph = new List<int>[numHouses + 1];
+        for (int i = 0; i < graph.Length; i++)
+            graph[i] = new();
+        for (int i = 0; i < numCables; i++)
+        {
+            string[] cable = Console.ReadLine().Split(' ');
+            int house1 = int.Parse(cable[0]);
+            int house2 = int.Parse(cable[1]);
+            graph[house1].Add(house2);
+            graph[house2].Add(house1);
+        }
+
+        List<int> noInternet = GetInternetless(graph);
+        if (noInternet.Count == 0)
+            Console.WriteLine("Connected");
+        else
+            foreach (int house in noInternet)
+                Console.WriteLine(house);
+    }
+
+    /// <summary>
+    /// Returns an ordered list of houses without internet
+    /// </summary>
+    /// <param name="graph">A 1-indexed graph where graph[i] is a list of houses with a connection to house i</param>
+    /// <returns></returns>
+    public static List<int> GetInternetless(List<int>[] graph)
+    {
+        // run DFS from house 1 to find houses with internet
+        bool[] hasInternet = new bool[graph.Length];
+        Stack<int> stack = new();
+        stack.Push(1);
+        while (stack.Count > 0)
+        {
+            int currHouse = stack.Pop();
+            // this one is already processed
+            if (hasInternet[currHouse])
+                continue;
+            hasInternet[currHouse] = true;
+            foreach (int connectedHouse in graph[currHouse])
+                stack.Push(connectedHouse);
+        }
+
+        List<int> noInternet = new();
+        for (int house = 1; house < hasInternet.Length; house++)
+            if (!hasInternet[house])
+                noInternet.Add(house);
+
+        return noInternet;
+    }
+}
